691757d1450d534ddf97a1ab6a41666d98cc82a3
Dan DeFelippi <dan@driverdan.com>
2019-01-21 21:31:46 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT dancrowdboticscom_dan_63.wsgi:application
---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from home.models import CustomText, HomePage
+
+
+def home(request):
+    packages = [
+	{'name':'djangorestframework', 'url': 'https://pypi.org/project/djangorestframework/3.9.0/'},
+	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
+    ]
+    context = {
+        'customtext': CustomText.objects.first(),
+        'homepage': HomePage.objects.first(),
+        'packages': packages
+    }
+    return render(request, 'home/index.html', context)
